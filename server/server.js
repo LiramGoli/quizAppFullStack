fc5f61e560ab4060c64e6ec187baee85c42f35fe
LiramGoli <83345475+LiramGoli@users.noreply.github.com>
2023-04-28 13:24:53 +0300

    server update

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
+    await connectDB("mongodb+srv://liram:liram@cluster0.3eiygld.mongodb.net/?retryWrites=true&w=majority");
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
