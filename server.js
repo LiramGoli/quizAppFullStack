beec039300639fd79c6d62981682fbbace9178ca
LiramGoli <83345475+LiramGoli@users.noreply.github.com>
2023-04-28 16:07:23 +0300

    server stuff

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 require("express-async-errors");
-const connectDB = require("./db/connect");
-const riddleRouter = require("./routes/collectRiddles");
+const serverless = require("serverless-http");
+const connectDB = require("./server/db/connect");
+const riddleRouter = require("./server/routes/collectRiddles");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -10,13 +11,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 //routes
+
 app.use("/api/v1/riddle-collect", riddleRouter);
 
 const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await connectDB("mongodb+srv://liram:liram@cluster0.3eiygld.mongodb.net/?retryWrites=true&w=majority");
+    await connectDB(
+      "mongodb+srv://liram:liram@cluster0.3eiygld.mongodb.net/?retryWrites=true&w=majority"
+    );
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
@@ -25,3 +29,4 @@ const start = async () => {
   }
 };
 start();
+module.exports.handler = serverless(app);
